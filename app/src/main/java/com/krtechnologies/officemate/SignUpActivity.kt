7153dfe67631de4dba2cedf61c2922d5f50b9a3f
kami_mehar38 <kamranramzan098@gmail.com>
2018-08-11 17:15:49 +0400

    Initial Commit

--- app/src/main/java/com/krtechnologies/officemate/SignUpActivity.kt
+++ app/src/main/java/com/krtechnologies/officemate/SignUpActivity.kt
@@ -1,5 +1,9 @@
 package com.krtechnologies.officemate
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.app.Activity
 import android.content.DialogInterface
 import android.graphics.PorterDuff
@@ -19,21 +23,36 @@ import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Message
 import android.util.Log
+import android.view.View
+import android.view.animation.AnimationSet
+import android.view.animation.OvershootInterpolator
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.Request
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.SizeReadyCallback
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
 import com.krprojects.aira.Aira
 import com.krtechnologies.officemate.helpers.Helper
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 
 
 class SignUpActivity : AppCompatActivity() {
 
     // properties
     private var mCurrentPhotoPath: String? = null
+    private var profileImage: Bitmap? = null
     private val PERMISION_CONSTANT: Int = 101
     private val REQUEST_IMAGE_CAPTURE = 1
     private val REQUEST_IMAGE_SELECT = 2
@@ -44,10 +63,17 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         ivProfilePicture.setOnClickListener {
             checkPermissionFirst()
         }
 
+        btnCancelProfilePicture.setOnClickListener {
+            if (profileImage != null) {
+                profileImage = null
+                ivProfilePicture.setImageDrawable(resources.getDrawable(R.drawable.person))
+            }
+        }
 
         etName.setOnFocusChangeListener { view, b -> if (b) Helper.getInstance().changeToAccent(view as EditText) else Helper.getInstance().changeToPrimary(view as EditText) }
         etEmail.setOnFocusChangeListener { view, b -> if (b) Helper.getInstance().changeToAccent(view as EditText) else Helper.getInstance().changeToPrimary(view as EditText) }
@@ -58,17 +84,66 @@ class SignUpActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         Aira.onActivityResult(requestCode)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            Glide.with(this)
-                    .load(mCurrentPhotoPath)
-                    .apply(RequestOptions().override(250, 250).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true))
-                    .into(ivProfilePicture);
+            // start cropping activity for pre-acquired image saved on the device
+            CropImage.activity(Uri.fromFile(photoFile))
+                    .setAspectRatio(1, 1)
+                    .setCropShape(CropImageView.CropShape.OVAL)
+                    .start(this)
+
             galleryAddPic()
         } else if (requestCode == REQUEST_IMAGE_SELECT && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
             toast(Helper.getInstance().getPath(data.data!!)!!)
+            // start cropping activity for pre-acquired image saved on the device
+            CropImage.activity(data.data)
+                    .setAspectRatio(1, 1)
+                    .setCropShape(CropImageView.CropShape.OVAL)
+                    .start(this)
+
+        } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            val result: CropImage.ActivityResult = CropImage.getActivityResult(data)
+            val resultUri: Uri = result.uri
             Glide.with(this)
-                    .load(Helper.getInstance().getPath(data.data!!))
-                    .apply(RequestOptions().override(250, 250).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).error(R.drawable.person).fallback(R.drawable.person))
-                    .into(ivProfilePicture);
+                    .asBitmap()
+                    .load(resultUri)
+                    .apply(RequestOptions().override(250, 250).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).fallback(R.drawable.person).error(R.drawable.person))
+                    .into(object : Target<Bitmap> {
+                        override fun onLoadStarted(placeholder: Drawable?) {
+                        }
+
+                        override fun onLoadFailed(errorDrawable: Drawable?) {
+                        }
+
+                        override fun getSize(cb: SizeReadyCallback) {
+                        }
+
+                        override fun getRequest(): Request? {
+                            return null
+                        }
+
+                        override fun onStop() {
+                        }
+
+                        override fun setRequest(request: Request?) {
+                        }
+
+                        override fun removeCallback(cb: SizeReadyCallback) {
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                        }
+
+                        override fun onStart() {
+                        }
+
+                        override fun onDestroy() {
+                        }
+
+                        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                            ivProfilePicture.setImageBitmap(resource)
+                            profileImage = resource
+                        }
+
+                    })
         }
     }
 
@@ -111,16 +186,18 @@ class SignUpActivity : AppCompatActivity() {
         alertDialogBuilder.create().show()
     }
 
+    var photoFile: File? = null
+
     // this function dispatches the intent to start the camera and capture image
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(packageManager) != null) {
             // Create the File where the photo should go
-            var photoFile: File? = null
+
             try {
                 photoFile = Helper.getInstance().createImageFile()
-                mCurrentPhotoPath = photoFile.absolutePath
+                mCurrentPhotoPath = photoFile?.absolutePath
             } catch (ex: IOException) {
 
             }
@@ -128,7 +205,7 @@ class SignUpActivity : AppCompatActivity() {
             if (photoFile != null) {
                 val photoURI: Uri = FileProvider.getUriForFile(this,
                         "com.krtechnologies.officemate.fileprovider",
-                        photoFile)
+                        photoFile!!)
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
             }
@@ -157,4 +234,37 @@ class SignUpActivity : AppCompatActivity() {
         Toast.makeText(this@SignUpActivity, message, Toast.LENGTH_SHORT).show()
     }
 
+    override fun onEnterAnimationComplete() {
+        super.onEnterAnimationComplete()
+        popUp()
+    }
+
+    private fun popUp() {
+
+        val scaleX = ObjectAnimator.ofInt(btnCancelProfilePicture, "scaleX", 0, 1)
+        val scaleY = ObjectAnimator.ofInt(btnCancelProfilePicture, "scaleY", 0, 1)
+
+        val animator = AnimatorSet()
+        animator.playTogether(scaleX, scaleY)
+        animator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                btnCancelProfilePicture.visibility = View.VISIBLE
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+
+            }
+        })
+        animator.interpolator = OvershootInterpolator()
+        animator.duration = 500
+        animator.start()
+    }
 }
