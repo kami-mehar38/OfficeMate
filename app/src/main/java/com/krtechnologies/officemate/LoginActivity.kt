c500bb44cb19342b0b6e2984930dc2ed933b850d
kami_mehar38 <kamranramzan098@gmail.com>
2018-08-12 17:04:24 +0400

    Initial Commit 12/Aug

--- app/src/main/java/com/krtechnologies/officemate/LoginActivity.kt
+++ app/src/main/java/com/krtechnologies/officemate/LoginActivity.kt
@@ -1,14 +1,29 @@
 package com.krtechnologies.officemate
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.TargetApi
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // making the status bar transparent
+        setStatusBarColor()
+
         setContentView(R.layout.activity_login)
 
         supportActionBar?.hide()
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun setStatusBarColor() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT
     }
 }
