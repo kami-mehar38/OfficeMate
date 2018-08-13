703d1454d5eef0ad64f6db4b5b85f61eb2a1b055
kami_mehar38 <kamranramzan098@gmail.com>
2018-08-13 16:54:39 +0400

    Initial Commit 13/Aug Row News feed

--- app/src/main/java/com/krtechnologies/officemate/fragments/NewsFeedFragment.kt
+++ app/src/main/java/com/krtechnologies/officemate/fragments/NewsFeedFragment.kt
@@ -1,41 +1,18 @@
 package com.krtechnologies.officemate.fragments
 
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.krtechnologies.officemate.R
+import android.R.attr.endColor
+import android.R.attr.startColor
+import com.ldoublem.ringPregressLibrary.Ring
+import kotlinx.android.synthetic.main.fragment_news_feed.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [NewsFeedFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [NewsFeedFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class NewsFeedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -43,39 +20,21 @@ class NewsFeedFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_news_feed, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
+    private var mCounter: Int = 0
 
-    /*override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }*/
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val r1 = Ring(50, "Hi there", "50%", startColor, endColor)
+        val r2 = Ring(50, "Hi there", "50%", startColor, endColor)
+        val r3 = Ring(50, "Hi there", "50%", startColor, endColor)
+        val r4 = Ring(50, "Hi there", "50%", startColor, endColor)
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
+        val listRing = ArrayList<Ring>()
+        listRing.add(r1)
+        listRing.add(r2)
+        listRing.add(r3)
+        listRing.add(r4)
+        ring_progress.setData(listRing, 1500)
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
@@ -92,8 +51,7 @@ class NewsFeedFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
                 NewsFeedFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+
                     }
                 }
     }
